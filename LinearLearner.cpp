e69ffc5f3fee6b1814935f880662cad97c67fe3b
user <16asampson@gmail.com>
2019-10-19 16:29:29 -0500

    added the base file for Program

---
+++ LinearLearner.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<list>
+
+using namespace std;
+
+int LinearLearner()
+{
+    //TODO: read input document
+
+
+    //TODO: get LearningRate from user
+    int LRate;
+    cin >> LRate;
+
+    //TODO Algorithm implementation
+
+
+}
